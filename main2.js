90c240b27476f44389a885009cbe4ca9108713ab
Takeallah <takeallahsrivera@icloud.com>
2020-02-18 13:24:17 -0600

    ClassworkPushOne

---
+++ main2.js
@@ -0,0 +1,7 @@
+let userName = 'Takeallah';
+
+alert(`Hello there ${userName}
+ Have a great day!!`);
+
+alert(`And if you dont have a great day ${userName} I hope you have a better one tomorrow ${userName}`);
+
---
+++ script.js
@@ -0,0 +1,10 @@
+let userName = prompt('What is your name:');
+
+if (userName == ""){
+    alert('Hello there  ' + userName);
+if (userName = !""){
+    alert('NO);
+}
+
+
+}
